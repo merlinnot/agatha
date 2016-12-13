4e709a0a037e79eee3a0a99d04520da40a62673e
Natan Sągol <m@merlinnot.com>
2016-12-13 21:52:41 +0000

    Caching fonts in service worker, logo, renaming modules, menu drawer, color palette.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,5 +4,9 @@ module.exports = {
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
+  runtimeCaching: [{
+    urlPattern: '/^https://fonts.googleapis.com//',
+    handler: 'cacheFirst'
+  }],
   navigateFallback: '/index.html'
 };
